20e8005998a554e31e927709bfdba60b93be95e9
frankirova <franciscor1905@gmail.com>
2023-02-07 14:44:30 -0300

    arreglando problema de archivo .env

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// import dotenv from '../node_modules/dotenv'
+// require('dotenv').config()
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
 // require('').config()
-// require('dotenv').config()
+// require(dotenv).config();
+// module.exports = {
+//   REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
+// };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -3,11 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
-
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY
 const firebaseConfig = {
   apiKey: REACT_APP_API_KEY,
-  // apiKey: REACT_APP_API_KEY,
   authDomain: "realtech-ecomerce-react-coder.firebaseapp.com",
   projectId: "realtech-ecomerce-react-coder",
   storageBucket: "realtech-ecomerce-react-coder.appspot.com",
